6147d6af1418dab2bb898196412646d418963d37
Ian Henry <ianhenry26@gmail.com>
2020-02-16 21:45:06 -0500

    Added flatlist

--- src/Navigator.tsx
+++ src/Navigator.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import {
-  createStackNavigator,
-  StackNavigationProp
-} from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
 // Import screens under here
 import HomeScreen from "./screens/HomeScreen";
 import ListScreen from "./screens/ListScreen";
